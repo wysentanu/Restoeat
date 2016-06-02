1133353d87c5b5bf51d2d86551e112fcc33afd3f
Surya Diputra <surya_dipt@yahoo.com>
2016-06-02 18:44:05 +0800

    Add Login

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -7,7 +7,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>Hotel Deluxe  A Hotel category Flat bootstrap Responsive  Website Template | Home :: w3layouts</title>
 	<link href="{{ asset("/css/bootstrap.css") }}" rel='stylesheet' type='text/css' />
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<!-- Custom Theme files -->
@@ -15,9 +14,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	<link href="{{ asset("/css/style0.css") }}" rel='stylesheet' type='text/css' />
 	<!-- Custom Theme files -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="Hotel Deluxe Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
-	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /?>
 	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 	<!--webfont-->
 	<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
