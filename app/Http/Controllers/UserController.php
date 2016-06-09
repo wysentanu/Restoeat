903d80d9211583ab2b653a2eef2ff459c5fee047
Ariantha Sentanu <wysentanu@gmail.com>
2016-06-09 20:10:22 +0800

    register fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller
         $this->middleware('auth', ['except' => [
             'login',
             'authentication',
+            'register',
         ]]);
     }
 
